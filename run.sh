82c097f88c877d65b0ac7df0397d264c6c3b6e69
Sebastian Lee <sebastian.lee@deepset.ai>
2023-01-20 14:17:07 +0100

    Add run.sh used for running training

---
+++ run.sh
@@ -0,0 +1,14 @@
+python src/run_tagger.py \
+    --model_name_or_path bert-base-uncased \
+    --data_dir ./data/MultiSpanQA_data \
+    --output_dir ./output \
+    --overwrite_output_dir \
+    --overwrite_cache \
+    --do_train \
+    --do_eval \
+    --per_device_train_batch_size 4 \
+    --eval_accumulation_steps 50 \
+    --learning_rate 3e-5 \
+    --num_train_epochs 3 \
+    --max_seq_length  512 \
+    --doc_stride 128 > run.out 2>&1
